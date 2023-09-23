27f1bc7783b8504aa3807407ff218d12154faf72
Jeongho Park <d0lbangi13@naver.com>
2023-09-23 16:35:16 +0900

    매매, 임대차 함수 로직 추가

--- src/main/java/com/d0lbangi/houseutils/controller/BrokerageQueryController.java
+++ src/main/java/com/d0lbangi/houseutils/controller/BrokerageQueryController.java
@@ -1,6 +1,10 @@
 package com.d0lbangi.houseutils.controller;
 
 import com.d0lbangi.houseutils.constants.ActionType;
+import com.d0lbangi.houseutils.policy.BrokeragePolicy;
+import com.d0lbangi.houseutils.policy.BrokeragePolicyFactory;
+import com.d0lbangi.houseutils.policy.PurchaseBrokeragePolicy;
+import com.d0lbangi.houseutils.policy.RentBrokeragePolicy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +20,8 @@ public class BrokerageQueryController {
     public Long CalcBrokerage(@RequestParam ActionType actionType,
                               @RequestParam Long price) {
         // TODO: 중개수수료 계산하는 로직
-        return null;
+        BrokeragePolicy policy = BrokeragePolicyFactory.of(actionType);
+        return policy.calculate(price);
     }
 }
 
---
+++ src/main/java/com/d0lbangi/houseutils/policy/BrokeragePolicyFactory.java
@@ -0,0 +1,21 @@
+package com.d0lbangi.houseutils.policy;
+
+import com.d0lbangi.houseutils.constants.ActionType;
+
+/**
+ * @author Happy
+ */
+
+public class BrokeragePolicyFactory {
+    public static BrokeragePolicy of(ActionType actionType) {
+        switch (actionType) {
+            case RENT:
+                return new RentBrokeragePolicy();
+            case PURCHASE:
+                return new PurchaseBrokeragePolicy();
+            default:
+                throw new IllegalArgumentException("해당 actiontype에 대한 정책이 존재하지 않습니다."); // 액션타입에 추가될 수 있으므로
+
+        }
+    }
+}
