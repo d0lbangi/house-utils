4e01a9ab3e03bdd3e8137bd441fb299b5364f334
Jeongho Park <d0lbangi13@naver.com>
2023-09-23 17:32:17 +0900

    캡슐화 클래스 추가

--- src/main/java/com/d0lbangi/houseutils/policy/BrokeragePolicy.java
+++ src/main/java/com/d0lbangi/houseutils/policy/BrokeragePolicy.java
@@ -1,5 +1,10 @@
 package com.d0lbangi.houseutils.policy;
 
+import com.d0lbangi.houseutils.exception.ErrorCode;
+import com.d0lbangi.houseutils.exception.HouseUtilsException;
+
+import java.util.List;
+
 /**
  * @author d0lbangi
  *
@@ -7,10 +12,15 @@ package com.d0lbangi.houseutils.policy;
  */
 public interface BrokeragePolicy {
 
-    BrokerageRule createBrokerageRule(Long price);
+    List<BrokerageRule> getRules();
 
     default Long calculate(Long price) {
-        BrokerageRule rule = createBrokerageRule(price);
-        return rule.calcMaxBrokerage(price);
+
+        // 가격에 따른 룰을 찾아서 가져오는 로직
+
+        BrokerageRule brokerageRule = getRules().stream()
+                .filter(rule -> price < rule.getLessThan())
+                .findFirst().orElseThrow(()-> new HouseUtilsException(ErrorCode.INTERNAL_ERROR));
+        return brokerageRule.calcMaxBrokerage(price);
     }
 }
--- src/main/java/com/d0lbangi/houseutils/policy/BrokeragePolicyFactory.java
+++ src/main/java/com/d0lbangi/houseutils/policy/BrokeragePolicyFactory.java
@@ -10,12 +10,17 @@ import com.d0lbangi.houseutils.exception.HouseUtilsException;
  */
 
 public class BrokeragePolicyFactory {
+    private static final  RentBrokeragePolicy rentBrokeragePolicy = new RentBrokeragePolicy();
+    private static final  PurchaseBrokeragePolicy purchaseBrokeragePolicy = new PurchaseBrokeragePolicy();
+
+
+
     public static BrokeragePolicy of(ActionType actionType) {
         switch (actionType) {
             case RENT:
-                return new RentBrokeragePolicy();
+                return rentBrokeragePolicy;
             case PURCHASE:
-                return new PurchaseBrokeragePolicy();
+                return purchaseBrokeragePolicy;
             default:
                 throw new HouseUtilsException(ErrorCode.INVALID_REQUEST, "해당 actiontype에 대한 정책이 존재하지 않습니다.");
 
